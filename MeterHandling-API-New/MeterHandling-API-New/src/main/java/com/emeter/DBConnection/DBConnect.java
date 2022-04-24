0422d4e2a3ab0330ad72c15ef9e7bf05a53e9de3
malshika <gpmalshika@gmail.com>
2022-04-24 17:06:53 +0530

    malki final commits

--- MeterHandling-API-New/MeterHandling-API-New/src/main/java/com/emeter/DBConnection/DBConnect.java
+++ MeterHandling-API-New/MeterHandling-API-New/src/main/java/com/emeter/DBConnection/DBConnect.java
@@ -1,5 +1,28 @@
 package com.emeter.DBConnection;
 
-public class DBConnect {
 
+import java.sql.*;
+
+public class DBConnect {
+	
+	public static Connection connect() {
+		
+	Connection con = null;
+	
+	try
+	{
+		
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/meter_handling_db","root", "malshika123");
+	//For testing
+	System.out.print("Successfully connected");
+	}
+	
+	catch(Exception e)
+	{
+	e.printStackTrace();
+	}
+	
+	return con;
+}
 }
